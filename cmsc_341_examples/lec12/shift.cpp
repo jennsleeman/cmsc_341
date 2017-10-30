f86451a0125b50a782e9a1b4cf25dc653a7e8955
Jennifer Sleeman <jsleem1@umbc.edu>
2017-10-30 14:55:54 -0400

    added lec12 dir

---
+++ cmsc_341_examples/lec12/shift.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include <iostream>
+
+int main() {
+      int n=4;
+      int s=4>>1;
+      int ss=4<<1;
+      cout << " n is " << 4 << endl;
+      cout << " n shifted right is " << s << endl;
+      cout << " n shifted left is " << ss << endl;
+}
